fafc27fa9fa0cd2a323c66feee9366a19aaac453
zackees <z@zackees.com>
2022-12-11 15:45:50 -0800

    fix get_log route

--- fastapi_template_project/log.py
+++ fastapi_template_project/log.py
@@ -34,43 +34,11 @@ def make_logger(name: str) -> Logger:
     return log
 
 
-def reverse_readline(filename, buf_size=8192):
-    """A generator that returns the lines of a file in reverse order"""
-    with open(filename, encoding="utf-8", mode="r") as filehandle:
-        segment = None
-        offset = 0
-        file_size = remaining_size = filehandle.tell()
-        while remaining_size > 0:
-            offset = min(file_size, offset + buf_size)
-            filehandle.seek(file_size - offset)
-            buffer = filehandle.read(min(remaining_size, buf_size))
-            remaining_size -= buf_size
-            lines = buffer.split("\n")
-            # The first line of the buffer is probably not a complete line so
-            # we'll save it and append it to the last line of the next buffer
-            # we read
-            if segment is not None:
-                # If the previous chunk starts right from the beginning of line
-                # do not concat the segment to the last line of new chunk.
-                # Instead, yield the segment first
-                if buffer[-1] != "\n":
-                    lines[-1] += segment
-                else:
-                    yield segment
-            segment = lines[0]
-            for index in range(len(lines) - 1, 0, -1):
-                if lines[index]:
-                    yield lines[index]
-        # Don't yield None if the file was empty
-        if segment is not None:
-            yield segment
-
-
 def get_log_reversed(num_lines: int) -> str:
     """Returns the log in reverse line order"""
     lines = []
     remaining_lines = num_lines
-    with FileReadBackwards("LOG_SYSTEM", encoding="utf-8") as frb:
+    with FileReadBackwards(LOG_SYSTEM, encoding="utf-8") as frb:
         for line in frb:
             if remaining_lines <= 0:
                 break
