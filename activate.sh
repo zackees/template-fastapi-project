8eb3754dd6c08684986b9e41ec0bc5db0bb77b9e
Zachary Vorhies <zachvorhies@protonmail.com>
2023-02-01 17:06:12 -0800

    Update activate.sh

--- activate.sh
+++ activate.sh
@@ -1,11 +1,17 @@
+
 #!/bin/bash
 set -e
 function abs_path {
   (cd "$(dirname '$1')" &>/dev/null && printf "%s/%s" "$PWD" "${1##*/}")
 }
+
 # if make_venv dir is not present, then make it
 if [ ! -d "venv" ]; then
   python make_venv.py
+fi
+# if IN_ACTIVATED_ENV is set, then we are already in the venv
+if [ -n "$IN_ACTIVATED_ENV" ]; then
+  return
 fi
 . $( dirname $(abs_path ${BASH_SOURCE[0]}))/venv/bin/activate
 export PATH=$( dirname $(abs_path ${BASH_SOURCE[0]}))/:$PATH
