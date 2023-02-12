837358ffaf478a60fc9d4ac4f9a2d128916e7114
zackees <z@zackees.com>
2023-02-11 16:24:00 -0800

    remove reload directory

--- entry_point.sh
+++ entry_point.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-uvicorn --host 0.0.0.0 --port 80 --reload --reload-dir restart --workers 8 --forwarded-allow-ips=* fastapi_template_project.app:app
+uvicorn --host 0.0.0.0 --port 80 --workers 8 --forwarded-allow-ips=* fastapi_template_project.app:app
