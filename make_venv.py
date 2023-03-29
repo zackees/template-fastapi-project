9034fded1ea7ca9a90145dbcd34829286e1c161d
Zachary Vorhies <zachvorhies@protonmail.com>
2023-03-29 14:29:27 -0700

    Update make_venv.py

--- make_venv.py
+++ make_venv.py
@@ -39,8 +39,6 @@ if [ -n "$IN_ACTIVATED_ENV" ]; then
 fi
 . $( dirname $(abs_path ${BASH_SOURCE[0]}))/venv/bin/activate
 export PATH=$( dirname $(abs_path ${BASH_SOURCE[0]}))/:$PATH
-alias python3=python
-alias pip3=pip
 export IN_ACTIVATED_ENV="1"
 """
 
