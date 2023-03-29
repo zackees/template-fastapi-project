d5d9d635f76cd189e0d31f2f208f0947923ee760
Zachary Vorhies <zachvorhies@protonmail.com>
2023-03-28 20:15:22 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import setuptools
 
 
 URL = "https://github.com/zackees/fastapi-template-project"
-
+KEYWORDS = ""
 HERE = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -30,6 +30,7 @@ def get_readme() -> str:
 if __name__ == "__main__":
     setuptools.setup(
         maintainer="Zachary Vorhies",
+        keywords=KEYWORDS,
         long_description=get_readme(),
         long_description_content_type="text/markdown",
         url=URL,
