f0bf93c2b168fe794a61416212dcb2e1f59cd37b
Zachary Vorhies <zachvorhies@protonmail.com>
2023-12-02 14:54:29 -0800

    Update lint.sh

--- lint.sh
+++ lint.sh
@@ -1,20 +1,19 @@
 #! /bin/bash
 
 set -e
-. ./activate.sh
-echo Running ruff src
-ruff --fix src
-echo Running ruff tests
-ruff --fix tests
+cd $( dirname ${BASH_SOURCE[0]})
+if [ -z "$GITHUB_ACTIONS" ]; then
+    . ./activate.sh
+fi
+echo Running ruff src tests
+ruff --fix src tests
+echo Running isort
+isort src tests
 echo Running black.
 black src tests
-echo Running isort src tests
-isort --profile black src tests
 echo Running flake8
 flake8 src tests
-echo Running pylint src
-pylint src
-echo Running pylint tests
-pylint tests  # seperate or crash happens in git-bash
+# echo Running pylint
+# pylint src  # seperate or crash happens in git-bash
 echo Running mypy
 mypy src tests
